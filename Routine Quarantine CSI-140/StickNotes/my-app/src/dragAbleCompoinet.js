b150548bb3f67ea7a5bdc8d8799a09473b1860a0
Joseph Centeno <joseph.centeno@mymail.champlain.edu>
2020-04-26 19:18:16 -0600

    My Final Project

---
+++ Routine Quarantine CSI-140/StickNotes/my-app/src/dragAbleCompoinet.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+ 
+class App extends React.Component {
+ 
+ 
+ 
+  render() {
+    return (
+      <Draggable
+        axis="x"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable>
+    );
+  }
+}
+ 
+ReactDOM.render(<App/>, document.body);
